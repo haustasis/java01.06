a7c161566af126c28e6019777c0c96f7abefa1cd
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-14 09:12:59 +0100

    ex2 clean

--- nivell1/src/n1exercici2/App.java
+++ nivell1/src/n1exercici2/App.java
@@ -3,7 +3,12 @@ package n1exercici2;
 public class App {
 
     public static void main(String[] args) {
-        // <T> : String + Integer + Boolean
-        System.out.println(GenericMethods.f("uno", 2, true));
+
+        System.out.println(GenericMethods.f("uno", "dos", "tres"));
+        System.out.println("---------------");
+        System.out.println(GenericMethods.f(1, 2, 3));
+        System.out.println("---------------");
+        System.out.println(GenericMethods.f(true, false, true));
+
     }
 }
