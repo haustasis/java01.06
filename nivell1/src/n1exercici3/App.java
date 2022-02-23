148fa88f54906459df126f4c63662dc8216ad4c4
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-23 10:03:25 +0100

    ex3 modificat annotacions profe Jordi

--- nivell1/src/n1exercici3/App.java
+++ nivell1/src/n1exercici3/App.java
@@ -4,11 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println(GenericMethods.f("uno", "dos", true));
-        System.out.println("---------------");
-        System.out.println(GenericMethods.f(1, 2, "treintaytres"));
-        System.out.println("---------------");
-        System.out.println(GenericMethods.f(true, false, "cuarentaytres"));
+        System.out.println(GenericMethods.f(1, true, 'c'));
 
     }
 }
--- nivell1/src/n1exercici3/GenericMethods.java
+++ nivell1/src/n1exercici3/GenericMethods.java
@@ -2,7 +2,7 @@ package n1exercici3;
 
 public class GenericMethods {
 
-    public static <T> String f(T first, T second, T third) {
+    public static <T> String f(T first, T second, Character third) {
         return "Primer element: " + first + "\nSegon element: " + second + "\nTercer element: " + third;
     }
 
